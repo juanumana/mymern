957adc696dbdaedf60ce7906032a7696a94ad342
juan <juangaumana@gmail.com>
2021-03-31 15:53:40 +0200

    get / post / delete commands on DB

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser =('body-parser');
 
+const items = require('./routes/api/items');
+
+
+
 const app= express();
 
 app.use(express.json());
@@ -14,6 +18,9 @@ mongoose
     .connect(db)
     .then( () => console.log('MongoDB connected'))
     .catch(err=> console.log(err));
+
+//use routes
+app.use('/api/items', items);
 
 const port = process.env.PORT || 5153;
 app.listen(port, () => console.log(`server startet on port: ${port}`));
